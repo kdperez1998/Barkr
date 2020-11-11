04c4e86a8b8dd1fa0eb861810c9d730a1b031168
Kaley <kdperez1998@hotmail.com>
2020-11-10 18:24:52 -0600

    Fixed Alexa's code. It is not actually fixed and finished, but now you can run tests without errors.

--- app/src/main/java/com/example/barkr/SearchActivityMain.java
+++ app/src/main/java/com/example/barkr/SearchActivityMain.java
@@ -7,6 +7,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
