523c7b96907161c4437fd106257aa4b2565eb974
SimonMeek1997 <73914953+SimonMeek1997@users.noreply.github.com>
2020-11-11 08:06:07 -0600

    Add files via upload
    In Progress

---
+++ app/src/main/User.java
@@ -0,0 +1,21 @@
+public class User {
+    String userName, Password;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void setuserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    public String getUsername() {
+        return this.userName;
+    }
+    public String getPassword() {
+        return this.Password;
+    }
+}
