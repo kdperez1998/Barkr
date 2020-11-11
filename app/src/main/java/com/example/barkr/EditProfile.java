9bb74eb6941c9f898bb9b8927e914293da187c12
Cindy <Crivas@uttyler.edu>
2020-11-10 18:29:23 -0600

    -

---
+++ app/src/main/java/com/example/barkr/EditProfile.java
@@ -0,0 +1,21 @@
+package com.example.barkr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+
+
+
+public class EditProfile extends AppCompatActivity {
+    EditText TextPostalAddress,editTextDate,TextPersonName,EditCity, EditEmail;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+
+    }
+
+}
