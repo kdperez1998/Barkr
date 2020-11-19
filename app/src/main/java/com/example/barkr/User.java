d5fe3b54fe740f9fb566637b4ec66044c215bca5
Alexa <56326976+AlexaJade@users.noreply.github.com>
2020-11-18 22:50:32 -0600

    User, HumanProfile, DogProfile - now are Java files and contain methods

---
+++ app/src/main/java/com/example/barkr/User.java
@@ -0,0 +1,23 @@
+package com.example.barkr;
+
+public class User {
+    String userName, Password;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void setuserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    public String getUsername() {
+        return this.userName;
+    }
+    public String getPassword() {
+        return this.Password;
+    }
+}
